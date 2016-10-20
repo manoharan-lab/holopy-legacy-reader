571637a4e462a64b0c3a4b737e44512450a46f58
Tom Dimiduk <tom@dimiduk.net>
2016-10-20 10:47:45 -0400

    Switch to using holopy's save to write the hdf5 files.

--- convert.py
+++ convert.py
@@ -1,14 +1,15 @@
 import serialize
 from holopy.core.metadata import Image
+from holopy.core import save
 import sys
 import os
+
 
 def convert_image_yaml(yaml):
     h = serialize.load(yaml)
     d = Image(h, spacing=h.spacing, medium_index=h.optics.index, illum_wavelen=h.optics.wavelen, illum_polarization=h.optics.polarization)
-    ds = d.to_dataset(name='data')
     name, ext = os.path.splitext(yaml)
-    ds.to_netcdf('{}.h5'.format(name), engine='h5netcdf')
+    save(name, d)
 
 
 if __name__ == '__main__':
